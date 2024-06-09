0e585da7d2e49539584a543884e752cef5b81e85
DAZ406 <davidikzeli@gmail.com>
2024-06-09 17:44:30 +0300

    feat(changed log file name): changed log file name

--- server/logger.ts
+++ server/logger.ts
@@ -3,5 +3,5 @@ import winston from 'winston';
 export const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.json(),
-  transports: [new winston.transports.File({ filename: 'users.log' })],
+  transports: [new winston.transports.File({ filename: 'requests.log' })],
 });
